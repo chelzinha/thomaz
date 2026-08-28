7187f4dbd425bc6637aa2a4f8a6676e90910669e
chelzinha <137534302+chelzinha@users.noreply.github.com>
2026-08-28 00:17:10 -0300

    chore: adiciona configuracao local segura

---
+++ runtime-config.js
@@ -0,0 +1,15 @@
+window.__AGENDA_CONFIG__ = {
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  },
+  familyId: 'thomaz-family',
+  adminEmail: 'chelzinha@gmail.com',
+  googleCalendar: {
+    calendarId: '9f6c20fbf4f23efc223d60b2fd64bbf056bb3fe85379e24e5f6912421f9d3554@group.calendar.google.com'
+  }
+};
