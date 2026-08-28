96a313a86b3288f7c7fb7f2080c584af58bf73f7
chelzinha <137534302+chelzinha@users.noreply.github.com>
2026-08-27 23:58:37 -0300

    chore: adiciona modelo seguro de configuração

---
+++ config.example.js
@@ -0,0 +1,18 @@
+window.THOMAZ_APP_CONFIG = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  google: {
+    calendarId: "",
+    clientId: ""
+  },
+  app: {
+    timezone: "America/Fortaleza",
+    familyName: "Família do Thomaz"
+  }
+};
